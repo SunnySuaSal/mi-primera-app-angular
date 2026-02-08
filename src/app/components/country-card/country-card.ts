d8246e0d598f6278e47b52793ddf3c1cc9b3a3b8
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2026-02-08 10:03:38 -0600

    Creates component for cournty card

---
+++ src/app/components/country-card/country-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-card',
+  imports: [],
+  templateUrl: './country-card.html',
+  styleUrl: './country-card.css',
+})
+export class CountryCard {
+
+}
