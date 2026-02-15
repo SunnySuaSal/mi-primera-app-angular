b08f1aaae62e33dbc99e26b2e0452133f552949c
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2026-02-15 16:29:09 -0600

    coloca inicialmente controls en content

--- src/app/components/content/content.ts
+++ src/app/components/content/content.ts
@@ -1,10 +1,11 @@
 import { Component, signal, WritableSignal } from '@angular/core';
 import { CountryCard } from '../country-card/country-card';
-import { Country } from '../../interfaces/country.interface'
+import { Country } from '../../interfaces/country.interface';
+import { Controls } from '../controls/controls';
 
 @Component({
   selector: 'app-content',
-  imports: [CountryCard],
+  imports: [CountryCard, Controls],
   templateUrl: './content.html',
   styleUrl: './content.css',
 })
