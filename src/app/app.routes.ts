1231eac219ae0f91e19c19927113ca7546624118
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2026-02-16 16:32:38 -0600

    Crea notfound page component y configura rutas para casos no contemplados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 import { AboutPage } from './pages/about-page/about-page';
 import { ContactPage } from './pages/contact-page/contact-page';
+import { NotFoundPage } from './pages/not-found-page/not-found-page';
 
 export const routes: Routes = [
   {path: 'home', component: HomePage},
   {path: 'about', component: AboutPage},
-  {path: 'contact', component: ContactPage}
+  {path: 'contact', component: ContactPage},
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "not-found", component: NotFoundPage },
+  { path: "**", redirectTo: "not-found", pathMatch: "full" }
 ];
