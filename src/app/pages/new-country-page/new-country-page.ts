2e8fb93a051f8937d2d8c538ce0e0c3d48a006a9
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2026-02-16 19:23:03 -0600

    Crea forms para conectar el formulario html con el ts

--- src/app/pages/new-country-page/new-country-page.ts
+++ src/app/pages/new-country-page/new-country-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-new-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-country-page.html',
   styleUrl: './new-country-page.css',
 })
@@ -15,4 +16,18 @@ export class NewCountryPage {
     'Oceania',
     'Africa'
   ];
+
+  form: FormGroup = new FormGroup({
+    name: new FormControl(),
+    capital: new FormControl(),
+    region: new FormControl(),
+    population: new FormControl(),
+    flag: new FormControl(),
+  })
+
+  submit(): void {
+    console.log("Formulario activado");
+    console.log(this.form);
+  }
+
 }
