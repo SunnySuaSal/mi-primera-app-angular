bb770953f900c8424f0be613389a52f44201ed95
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2026-02-17 16:34:29 -0600

    Agrega validacion de formulario

--- src/app/pages/new-country-page/new-country-page.ts
+++ src/app/pages/new-country-page/new-country-page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-country-page',
@@ -18,16 +18,21 @@ export class NewCountryPage {
   ];
 
   form: FormGroup = new FormGroup({
-    name: new FormControl(),
-    capital: new FormControl(),
-    region: new FormControl(),
-    population: new FormControl(),
-    flag: new FormControl(),
+    name: new FormControl('', [Validators.required]), //Default value and validation array
+    capital: new FormControl('', [Validators.required]), //Si un campo requerido no se llena, su propiedad valid cambia a false
+    region: new FormControl('Americas', [Validators.required]), //Con que un campo no sea valid, todo le form es invalido
+    population: new FormControl('', [Validators.required]), //otro tipo de validaciones son una longitud minima o maxima
+    flag: new FormControl('', [Validators.required]), //que sea un email, fecha o que cumpla con una expresion regular
   })
 
   submit(): void {
     console.log("Formulario activado");
-    console.log(this.form);
+    if(this.form.valid){
+      console.log("Formulario valido");
+
+      //creacion del elemento
+
+    }
   }
 
 }
