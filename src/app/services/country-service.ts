46b271c1a5ec19d331ef489e4d66589601fa3a5d
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2026-05-14 02:40:02 -0600

    Agrega cambios a servicio

--- src/app/services/country-service.ts
+++ src/app/services/country-service.ts
@@ -1,15 +1,23 @@
 import { Injectable, signal } from '@angular/core';
 import { Country } from '../interfaces/country.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountryService {
+
+  private http = inject(HttpClient);
+
   title = signal("Hola mundo");
 
   private _countries = signal<Country[]>([]);
 
   public countries = this._countries.asReadonly(); //Para leer
+
+  fetchCountries(): void {
+    this.http.get()
+  }
 
   public orderByName(): void {
     this._countries.update( countries =>
